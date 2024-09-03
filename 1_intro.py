c83b8382d82cfdde019f458fead46f70157de394
Pamelakristy <pamelakristyaroyo@gmail.com>
2024-09-03 12:34:24 +0700

    Perkuliahan 1

---
+++ 1_intro.py
@@ -0,0 +1,20 @@
+# Deklarasi variabel
+a = float (10.5)
+b = 5
+c = a + b
+perkalian = a * b 
+pembagian = a / b
+pengurangan = a - b
+modulo = a % b
+pangkat = a ** b
+
+# Operasi (operation)
+print(a)
+print(b)
+print(c)
+print(perkalian)
+print(pembagian)
+print(pengurangan)
+print(modulo)
+print(pangkat)
+ 
